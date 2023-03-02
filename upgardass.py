8edf7ace0c9060f18330c25fa425a10b70147d12
Anaghacha <99277507+Anaghacha@users.noreply.github.com>
2023-03-02 21:46:17 +0530

    Create upgardass.py

---
+++ upgardass.py
@@ -0,0 +1,10 @@
+
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the annual interest rate: "))
+time = float(input("Enter the time period in years: "))
+
+
+interest = (principal * rate * time) / 100
+
+
+print("Simple interest = ", interest)
